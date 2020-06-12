515d029ae4ac3c24da60ff478de4e6e168ab2fac
Ben <7274659+be0x0@users.noreply.github.com>
2020-06-11 22:52:04 -0400

    Testing instructions. Set to toggle acc forever.

--- prog_mem.v
+++ prog_mem.v
@@ -9,15 +9,23 @@ module prog_mem(
         line <= {addr,mem[addr]};
     end
 
+    //Program memory goes here. Commented out are previous tests.
     always @(*) begin
         if(!rst) begin
-            mem[0] = {2'h0,4'h5,12'd1,12'b0,12'b0}; //Add 1
-            mem[1] = {2'h0,4'h5,12'd2,12'b0,12'b0}; //Add 2
-            mem[2] = {2'h0,4'h5,12'd3,12'b0,12'b0}; //Add 3
-            mem[3] = {2'h0,4'h5,12'd4,12'b0,12'b0}; //Add 4
-            mem[4] = {2'h0,4'h5,12'd5,12'b0,12'b0}; //Add 5
-            mem[5] = {2'h0,4'h5,12'd6,12'b0,12'b0}; //Add 6
-            mem[6] = {2'h0,4'h5,12'd7,12'b0,12'b0}; //Add 7
+
+            //Toggle acc every two cycles.
+            mem[0] = {2'h0,4'h1,12'd150,12'h801,12'b0}; //Set acc to 150
+            mem[1] = {2'h0,4'h0,12'd0,12'b0,12'b0}; //Nop
+            mem[2] = {2'h0,4'h8,12'd0,12'b0,12'b0}; //Not
+            mem[3] = {2'h0,4'h2,12'd1,12'b0,12'b0}; //Jump to instruction 1 
+            /*
+            mem[0] = {2'h0,4'h5,12'd11,12'b0,12'b0}; //Add 11
+            mem[1] = {2'h0,4'h6,12'd140,12'b0,12'b0}; //subtract 140
+            mem[2] = {2'h0,4'h7,12'd3,12'b0,12'b0}; //Multiply by 3
+            mem[3] = {2'h0,4'h1,12'd1,12'h801,12'b0}; //move 1 to acc
+            mem[4] = {2'h0,4'h7,12'd2,12'b0,12'b0}; //Multiply by two
+            mem[5] = {2'h0,4'h2,12'h004,12'b0,12'b0}; //Jump to instruction 4
+            mem[6] = {2'h0,4'h5,12'd999,12'b0,12'b0}; //Add 999 */
         end
     end
 endmodule
