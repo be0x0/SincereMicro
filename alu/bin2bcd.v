a051c0e8a72e296722e52a6ed9eac8ef5ccccebb
Ben Ellis <benx45h@gmail.com>
2020-10-01 12:37:22 -0400

    Untested dgt/bin2bcd implementations

--- alu/bin2bcd.v
+++ alu/bin2bcd.v
@@ -1,13 +1,13 @@
 module bin2bcd(input signed [10:0] bin, output reg signed [12:0] bcd);
+  reg i;
+  reg j;
   always @(bin) begin
     for(i = 0; i < 12; i=i+1)
       bcd[i] <= 0;
     for(i = 0; i < 12; i++) begin
       for(j = 0; j < 3; j++)
-        if(bcd[(4*j+3)-:3)] > 4)
+        if(bcd[(4*j+3)-:3] > 4)
           bcd[4*j+3:4*j] <= bcd[4*j+3:4*j] + 3;
-      bcd = bcd[];
-
     end
   end
 endmodule
--- alu/dgt.v
+++ alu/dgt.v
@@ -1,6 +1,18 @@
 module dgt(acc, arg1, out);
   input signed [10:0] acc;
+  input signed [10:0] arg1;
   output wire signed [10:0] out;
 
-  //todo
+  wire bcd;
+  wire bin;
+ 
+  bin2bcd UBCD(.bin(acc), .bcd(bcd));
+
+  always @(*) begin
+    if(arg1 > 0 && arg1 < 3)
+      out <= bcd[4*arg1+:4];
+    else
+      out <= 999;
+  end
+
 endmodule
