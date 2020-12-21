934b35ab9a65b4a9efb4b1641c0be037cde75cbb
Ben Ellis <benx45h@gmail.com>
2020-12-21 11:45:47 -0500

    Added formal verification for subtractor

--- alu/sub.v
+++ alu/sub.v
@@ -1,14 +1,28 @@
 module sub(
   input signed [10:0] arg1, acc,
-  output reg [10:0] out);
+  output signed reg [10:0] out);
 
   wire signed [11:0] tmp;
 
   assign tmp = acc - arg1;
 
   always @(*) begin //overflow handler
-    if(tmp>999) out = 999;
-    else if(tmp<-999) out = -999;
+    if(tmp > 999) out = 999;
+    else if(tmp < -999) out = -999;
     else out = tmp[10:0];
   end
+
+`ifdef FORMAL
+    always @(*) begin
+        assert(out <= 999);
+        assert(out >= -999);
+
+        if(acc - arg1 > 999)
+            assert(out == 999);
+        else if(acc - arg1 < -999)
+            assert(out == -999);
+        else
+            assert(out == acc - arg1);
+    end
+`endif
 endmodule
