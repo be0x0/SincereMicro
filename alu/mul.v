26389d6532efc2c11e0e2307f467bc71450a460d
Ben Ellis <benx45h@gmail.com>
2020-12-21 12:05:06 -0500

    Added formal verification to multiplier

--- alu/mul.v
+++ alu/mul.v
@@ -1,15 +1,30 @@
 module mul(acc, arg1, out);
-  input signed [10:0] acc;
-  input signed [10:0] arg1;
+    input signed [10:0] acc;
+    input signed [10:0] arg1;
 
-  output reg signed [10:0] out;
+    output reg signed [10:0] out;
 
-  wire signed [21:0] tmp;
+    wire signed [21:0] tmp;
   
-  assign tmp = acc * arg1;
-  always @(*) begin
-    if(tmp > 999) out = 999;
-    else if(tmp < -999) out = -999;
-    else  out = tmp[10:0];
-  end
+    assign tmp = acc * arg1;
+
+    always @(*) begin
+        if(tmp > 999) out = 999;
+        else if(tmp < -999) out = -999;
+        else  out = tmp[10:0];
+    end
+
+`ifdef FORMAL
+    always @(*) begin
+        assert(out <= 999);
+        assert(out >= -999);
+
+        if(acc * arg1 > 999)
+            assert(out == 999);
+        else if(acc * arg1 < -999)
+            assert(out == -999);
+        else
+            assert(out == acc * arg1);
+        end
+`endif
 endmodule
