f970ffbde3fc8939b3a9ecb4eb49d6f9d522772b
Ben Ellis <benx45h@gmail.com>
2020-12-24 14:50:10 -0500

    integrated dst

--- alu/alu.v
+++ alu/alu.v
@@ -9,15 +9,14 @@ module alu(
   input wire [10:0] arg1, arg2, acc,
   output reg [10:0] out);
   
-  wire signed [10:0] add_out, sub_out, mul_out, not_out, dgt_out;
-  //signed reg dst_out;
+  wire signed [10:0] add_out, sub_out, mul_out, not_out, dgt_out, dst_out;
 
   add UADD(.arg1(arg1), .acc(acc), .out(add_out));
   sub USUB(.arg1(arg1), .acc(acc), .out(sub_out));
   mul UMUL(.arg1(arg1), .acc(acc), .out(mul_out));
   notop UNOT(.acc(acc), .out(not_out));
   dgt UDGT(.arg1(arg1), .acc(acc), .out(dgt_out));
-  //dst UDST(.arg1(arg1),.arg2(arg2),.acc(acc),.out(dst_out))
+  dst UDST(.arg1(arg1),.arg2(arg2),.acc(acc),.out(dst_out))
 
   always @(*) begin
     case(inst)
@@ -26,7 +25,7 @@ module alu(
       7 : out = mul_out;
       8 : out = not_out;
       9 : out <= dgt_out;
-      //10 : out <= dst_out;
+      10 : out <= dst_out;
       default: out = acc;
     endcase
   end
